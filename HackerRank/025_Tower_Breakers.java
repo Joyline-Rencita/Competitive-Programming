0f00fa7dee81ac40b2323f27bb0c12cc1717b4d2
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-10-13 20:12:16 +0530

    Create 025_Tower_Breakers.java

---
+++ HackerRank/025_Tower_Breakers.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Result {
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+     
+    public static int towerBreakers(int n, int m) {
+        // If all towers are of height 1, Player 2 always wins
+        if (m == 1) {
+            return 2;
+        }
+        
+        // If the number of towers is even, Player 2 wins
+        // If the number of towers is odd, Player 1 wins
+        return (n % 2 == 0) ? 2 : 1;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = Result.towerBreakers(n, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
